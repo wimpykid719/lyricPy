ccc565852b269f41468234725971979e4837a425
“wataru” <ltomu.web@gmail.com>
2021-02-03 20:31:41 +0900

    get lyric from genius done

--- lyrics.py
+++ lyrics.py
@@ -6,16 +6,14 @@ from bs4 import BeautifulSoup, Comment
 #Lyrics__Container-sc-1ynbvzw-2
 
 class Lyric():
-	def __init__(self, artist, song_name, max_recur=3):
+	def __init__(self, artist, song_name, max_recur=2):
 		self.artist = artist
 		self.song_name = song_name
 		self.__gtask = []
 		self.__canceled = False
 		self.__lyric = ''
+		#何回再帰的に行うか。リクエスト5と再帰3で合計15回までリクエストを投げる。
 		self.__max_recur = max_recur
-
-
-		
 		self.genius_url = self.make_genius_url(artist, song_name)
 		lyric = self.lyric_from_genius(self.genius_url, self.__max_recur)
 		print(lyric)
@@ -36,7 +34,8 @@ class Lyric():
 			return False
 	
 	def scrape_genius(self, url):
-		soup = self.get_soup(url)
+		if not self.__canceled:
+			soup = self.get_soup(url)
 		if soup and not self.__canceled:
 			lyric_soup = soup.select('.song_body-lyrics .lyrics p')
 			if lyric_soup:
@@ -58,6 +57,7 @@ class Lyric():
 			self.__gtask.cancel()
 
 
+
 	def lyric_from_genius(self, url, depth):
 		print('再帰の回数確認depth: ', self.__max_recur - depth)
 
@@ -65,20 +65,20 @@ class Lyric():
 			async def get_lyric_soup(url):
 				await self.loop.run_in_executor(None, self.scrape_genius, url)
 			
-			#main_loopの処理		
-			for _ in range(1):#タスクキャンセルいれてもここでエラーにならないのか?...キャンセルしたタスクに新しいタスクが追加される？
-				self.__gtask += [get_lyric_soup(url)]
-			return await asyncio.gather(*self.__gtask)
+			#main_loopの処理
+			tasks = [get_lyric_soup(url) for _ in range(5)]#ここでself.__gtaskを上書きしているのが原因だった
+			self.__gtask = asyncio.gather(*tasks)
+			return await self.__gtask
 		try:
 			self.loop = asyncio.get_event_loop()
 			self.loop.run_until_complete(main_loop(url))
-		except asyncio.exceptions.CancelledError as e:
+		except asyncio.CancelledError as e:
 			print("*** CancelledError ***", e)
 		finally:
 			if self.__lyric or depth <= 0:
 				return self.__lyric
 			else:
-				print('2回のリクエストで曲情報が取れなかった。')
+				print('5回のリクエストで曲情報が取れなかった。')
 				return self.lyric_from_genius(url, depth - 1)
 		
 		
